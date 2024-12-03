1408b2867962a6b94d7e0feb67b524931e5c9cfa
LudvikHA <ludvik.hoibjerg-aslaksen@nmbu.no>
2024-12-03 14:49:14 +0100

    Complete day_3

--- day_3_part_2.py
+++ day_3_part_2.py
@@ -24,31 +24,21 @@ def calculate_time(func):
 
 @calculate_time
 def my_code():
-    dont_finder = re.compile(r"don't[(][)]")
-    do_finder = re.compile(r"do[(][)]")
+    finder = re.compile(r"(mul[(][0-9]+,[0-9]+[)]|don't[(][)]|do[(][)])")
+    number_finder = re.compile(r"mul[(]([0-9]+),([0-9]+)[)]")
     total_sum = 0
+    multiplying = True
     with open("input_part_2.txt", "r") as infile:
         for lines in infile:
-            do_blocks = re.split(do_finder, lines)
-            print(do_blocks)
-            for blocks in do_blocks:
-                dont_match = re.search(dont_finder, blocks)
-                if dont_match == None:
-                    total_sum += finding_muls(blocks)
-                else:
-                    dont_block = dont_match.span()
-                    # print(dont_block[0])
-                    # print(blocks[:dont_block[0]])
-                    total_sum += finding_muls(blocks[:dont_block[0]])
-        print(total_sum)
+            data = re.findall(finder, lines)
+            for info in data:
+                if info == "don't()":
+                    multiplying = False
+                elif info == "do()":
+                    multiplying = True
+                elif "mul" in info and multiplying == True:
+                    numbers = re.findall(number_finder, info)
+                    total_sum += int(numbers[0][0])*int(numbers[0][1])
+    print(f"The total sum is {total_sum}")
 
-
-def finding_muls(line):
-    mul_finder = re.compile(r"mul[(]([0-9]+),([0-9]+)[)]")
-    values_to_mul = re.findall(mul_finder, line)
-    mul_sum = 0
-    for pairs in values_to_mul:
-        mul_sum = mul_sum + int(pairs[0])*int(pairs[1])
-    return mul_sum
 my_code()
-#Last value 83942127
