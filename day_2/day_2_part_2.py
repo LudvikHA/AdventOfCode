de195c07eb503b085e3a6297e2f2dec889a81d6f
LudvikHA <ludvik.hoibjerg-aslaksen@nmbu.no>
2024-12-03 12:03:40 +0100

    First draft day part 2

--- day_2/day_2_part_2.py
+++ day_2/day_2_part_2.py
@@ -24,34 +24,63 @@ def calculate_time(func):
 @calculate_time
 def my_code():
     reports = 0
-    with open("input_part_1.txt", "r") as infile:
+    with open("input_part_2.txt", "r") as infile:
         for line in infile:
             single_report = np.int64(line.split())
-            diff = difference(single_report)
-            increase = increasing(single_report)
-            decrease = decreasing(single_report)
+            report_difference = single_report[1:]-single_report[:-1]
 
-            if diff == False:
-                pass
-            if increase == False:
-                pass
-            if decrease == False:
-                pass
+            diff = difference(report_difference)
+            increase = increasing(report_difference)
+            decrease = decreasing(report_difference)
+
+            #Difference can be used to find decreasing and increasing. 
+            #If all numbers negative then decreasing. 
+            #If all numbers positive increasing
+            print(f"diff: {diff}, increase: {increase}, decrease: {decrease}")
+            dampner = False #True if a value has been removed
+
+            #Removes the first instance of diff that that is not 1, 2, 3
+            if diff == False and dampner == False:    
+                for index, numbers in enumerate(np.abs(report_difference)):
+                    if numbers > 3 or numbers == 0:
+                        report_difference = np.delete(report_difference, index)
+                        diff = difference(report_difference)
+                        dampner = True
+                    
+            if increase == False and dampner == False and decrease == False:
+                for index, numbers in enumerate(report_difference):
+                    if numbers < 0:
+                        report_difference = np.delete(report_difference, index)
+                        increase = increasing(report_difference)
+                        dampner = True
+                        if increase == False:
+                            report_difference = np.insert(report_difference, index, numbers)
+                        break
+                             
+            if decrease == False and dampner == False and increase == False:
+                for index, numbers in enumerate(report_difference):
+                    if numbers > 0:
+                        report_difference = np.delete(report_difference, index)
+                        decrease = decreasing(report_difference)
+                        dampner = True
+                        if decrease == False:
+                            report_difference = np.insert(report_difference, index, numbers)
+                        break
 
             if diff and (increase or decrease):
                 reports += 1
 
     print(reports)
 
-def difference(my_array):
-    #Checks if the differnce between two numbers are 1, 2 or 3. Returns
-    return not (np.any((np.abs(my_array[1:] - my_array[:-1]) > 3)) or np.any((np.abs(my_array[1:] - my_array[:-1]) == 0)))
+def difference(diff_report):
+    #Checks if the differnce between two numbers are 1, 2 or 3. 
+    return not (np.any(np.abs(diff_report) > 3) or np.any(np.abs(diff_report) == 0))
 
-def increasing(my_array):
+def increasing(diff_report):
         #Checks if the values are increasing
-        return np.all(my_array[1:] >=  my_array[:-1])
+        return np.all(diff_report > 0)
 
-def decreasing(my_array):
+def decreasing(diff_report):
         #Checks if the values are increasing
-        return np.all(my_array[1:] <=  my_array[:-1])
+        return np.all(diff_report < 0)
 my_code()
