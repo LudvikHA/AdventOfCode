ffb358f6f9828014d1229ea15b0763ed8bb4d162
Ludvik Høibjerg-Aslaksen <ludvik253@gmail.com>
2024-12-05 09:55:32 +0100

    day 5 part 2 finds all invalid updates

--- day_5/day_5_part_2.py
+++ day_5/day_5_part_2.py
@@ -22,8 +22,54 @@ def calculate_time(func):
 
     return inner1
 
+
+def convert_str(updates_unconverted: list[str]) -> list[int]:
+    updates_converted = []
+    for values_to_convert in updates_unconverted:
+        updates_converted.append(int(values_to_convert))
+    return updates_converted
+
 @calculate_time
 def my_code():
+    rules_ordering = True
+    rules = []
+    updates = []
     with open("input_part_2.txt", "r") as infile:
-        pass
+        for lines in infile:
+            lines = lines.strip()
+            if lines == "":
+                rules_ordering = False
+            else:
+                if rules_ordering == False:
+                    appendable_unconverted = lines.split(",")
+                    appendable_converted = convert_str(appendable_unconverted)
+                    updates.append(appendable_converted)
+
+                else:
+                    appendable_unconverted = lines.split("|")
+                    appendable_converted = convert_str(appendable_unconverted)
+                    rules.append(appendable_converted)
+
+        page_validity = True
+        middle_value = []
+        invaid_updates = []
+        for page_updates in updates:
+            for page_number in page_updates:
+                if page_validity == True:
+                    current_update_rules = [current_rules for current_rules in rules if page_number in current_rules]
+                    for rule in current_update_rules:
+                        try:
+                            if page_updates.index(rule[0]) < page_updates.index(rule[1]):
+                                page_validity = True
+                            else:
+                                page_validity = False
+                                break
+                        except ValueError:
+                            pass
+                else:
+                    break
+            if page_validity == False:
+                invaid_updates.append(page_updates)
+            print(invaid_updates)
+            page_validity = True
 my_code()
