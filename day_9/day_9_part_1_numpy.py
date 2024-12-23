d19d3d5a2a16d639fe4b5aedd357b8e04a68561e
Ludvik Høibjerg-Aslaksen <Ludvik253@gmail.com>
2024-12-23 23:27:05 +0100

    Day 9 part 1 correct result

--- day_9/day_9_part_1_numpy.py
+++ day_9/day_9_part_1_numpy.py
@@ -19,14 +19,11 @@ def calculate_time(func):
         print(f"Total time taken in : {func.__name__} {end - begin:.6f}")
     return inner1
 
-
-
-
 @calculate_time
 def my_code():
     with open("input_part_1.txt", "r") as infile:
         data = infile.readline()
-        disk_map = np.int32([number for number in data])
+        disk_map = np.int64([number for number in data])
         ID = 0
         formatted_disk_map = []
 
@@ -42,30 +39,28 @@ def my_code():
 
         #Moves the rightmost file to the first empty spaces from the left side
         formatted_disk_map = np.array(formatted_disk_map)
+        reversed_disk_map = np.flip(formatted_disk_map)
+        reversed_disk_map = reversed_disk_map[reversed_disk_map != "."]
+        
         altered_disk_map = np.copy(formatted_disk_map)
-        complete = False
-        free_spaces = len(formatted_disk_map[formatted_disk_map != "."])
-        for index_formatted, block_component in enumerate(formatted_disk_map[::-1]):
-            if complete == True:
-                break
-            elif block_component != ".":
-                for index_altered, block_altered in enumerate(altered_disk_map):
-                    if np.all(altered_disk_map[:free_spaces]!="."):
-                        complete = True
-                        break
-                        
-                    elif block_altered == ".":
-                        altered_disk_map[index_altered] = block_component
-                        altered_disk_map[-(index_formatted+1)] = "."
-                        break
+        free_spaces = np.where(formatted_disk_map==".")[0]
+        amount_of_free_spaces = len(free_spaces)
+        for index_free, blocks_free in enumerate(free_spaces):
+            altered_disk_map[blocks_free] = reversed_disk_map[index_free]
+        altered_disk_map = altered_disk_map[:-amount_of_free_spaces]
+
+        # print("0099811188827773336446555566..............")
+        # testing = ""
+        # for block_test in altered_disk_map:
+        #     testing = testing + block_test
+        # print(testing)
+
+        # print("Ho",altered_disk_map)
+
         checksum = 0
         for index_checksum, ID_cheksum in enumerate(altered_disk_map):
             if ID_cheksum == ".":
                 break
             checksum += index_checksum*int(ID_cheksum)
         print(checksum)
-
-        
-
-
 my_code()
